60ab8bccc3cf5c4992d7e73b4643f15180a93417
sthiti10 <sthiti.10@gmail.com>
2021-08-02 13:03:54 +0530

    4 files modified, 2 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -7,7 +7,7 @@ import { Product } from '../products';
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
-  @Input() productId!: Product;
+  @Input() product!: Product;
   @Output() notify = new EventEmitter();
 
   constructor() {}
