574845d9f9e2a667c69f94a09db8d0e18928b72e
sthiti10 <sthiti.10@gmail.com>
2021-08-02 15:05:13 +0530

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -8,7 +8,16 @@ import { products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
   product = products;
+  //product: Product|undefined;
   constructor() {}
-
+  //constructor(private route: ActivatedRoute,){}
   ngOnInit() {}
+  // ngOnInit() {
+  //   // First get the product id from the current route.
+  //   const routeParams = this.route.snapshot.paramMap;
+  //   const productIdFromRoute = Number(routeParams.get('productId'));
+
+  //   // Find the product that correspond with the id provided in route.
+  //   this.product = products.find(product => product.id === productIdFromRoute);
+  // }
 }
