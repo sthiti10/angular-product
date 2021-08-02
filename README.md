32a63f2e8f5f646ac10cdc215f3b4024bc41e842
sthiti10 <sthiti.10@gmail.com>
2021-08-02 11:57:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-wxxuzk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-wxxuzk)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  product = products;
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+  onNotify(){
+    window.alert('The product is on sale!');
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
